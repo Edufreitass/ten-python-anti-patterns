c459d814133d979514864f9ad724d738bbd903cf
Eduardo Freitas <eduardoflorencio96@gmail.com>
2023-07-25 14:14:28 -0300

    6 - Missing Docstrings

---
+++ app/src/6_missing_docstrings/bad_practice.py
@@ -0,0 +1,13 @@
+"""
+6 — Missing Docstrings
+
+Although there is not much to mention here, but, writing clear and concise
+docstrings is essential in Python. Instead of using random comments that
+describe a function, we can follow a specific docstring format like Numpy to
+improve our code readability and organization.
+"""
+
+
+# Some code without comments or explanations
+def my_function():
+    pass
---
+++ app/src/6_missing_docstrings/good_practice.py
@@ -0,0 +1,10 @@
+"""
+Tip 💡: Don’t Forget to update Docstring after Argument change!!
+"""
+
+
+def my_function():
+    """
+    This is a function that performs some operation.
+    """
+    pass
